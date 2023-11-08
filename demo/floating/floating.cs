9785f748e2390dd08bfdb5d7310be6fcfb35193c
Dennis van Oosterhout <dennis.vanoosterhout@cm.com>
2023-11-08 20:40:33 +0100

    Fixed float mechanism

--- demo/floating/floating.cs
+++ demo/floating/floating.cs
@@ -3,7 +3,18 @@ using System;
 
 public partial class floating : Sprite2D
 {
+    [Export]
+    public Vector2 Placement;
+
+    [Export]
+    public int Oscilliation;
+
     public override void _Ready() {
-        GD.Print("Hello from C# to Godot :)");
+        Position = Placement;
+    }
+
+    public override void _Process(double delta)
+    {
+        Position = Placement with { Y = Placement.Y + ((float) Math.Sin(Time.GetTicksMsec() / 1000f) * Oscilliation) };
     }
 }
