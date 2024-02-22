59223073fb880a86be179f14059996b934f8b54a
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2024-02-22 18:27:12 +0100

    Adapt to smaller screen

--- demo/custom_drawing/custom_drawing.cs
+++ demo/custom_drawing/custom_drawing.cs
@@ -9,10 +9,10 @@ public partial class custom_drawing : Node2D
   {
     Color WHITE = new(1, 1, 1);
 
-    DrawLine(new(500, 500), new (550, 530), WHITE, 2, true);
+    DrawLine(new(200, 200), new (250, 230), WHITE, 2, true);
     DrawArc(new(100, 100), 50, 0, 2 * MathF.PI, 360, WHITE, 2, true);
-    DrawCircle(new(500, 100), 30, WHITE);
-    DrawPolyline(new Vector2[] { new(100, 500), new (200, 500), new(150, 600), new(100, 500)}, WHITE, 2, true);
+    DrawCircle(new(300, 100), 30, WHITE);
+    DrawPolyline(new Vector2[] { new(600, 200), new (700, 200), new(650, 300), new(600, 200)}, WHITE, 2, true);
 
     // This should not work after export, though I did find it functional.
     // var image = Image.LoadFromFile("res://icon.svg");
@@ -22,7 +22,7 @@ public partial class custom_drawing : Node2D
     var texture = GD.Load<Texture2D>("res://icon.svg");
 
     DrawPrimitive(
-      new Vector2[]{ new(700, 700), new(800, 700), new(800, 800), new(700, 800) },
+      new Vector2[]{ new(300, 300), new(400, 300), new(400, 400), new(300, 400) },
       new[]{ WHITE },
       new Vector2[] { new(0,0), new(0.5f,0), new(0.5f, 0.5f), new(0,.5f)},
       texture
